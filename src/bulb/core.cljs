64c648adf1edecf14f7851df1b76d5f9f5af7b45
Keigo Suzukawa <k.suzukawa@gmail.com>
2019-09-23 19:20:27 +0100

    Minor tweaks

--- src/bulb/core.cljs
+++ src/bulb/core.cljs
@@ -1,20 +1,14 @@
 (ns bulb.core
   (:require [reagent.core :as reagent]
-            [re-frame.core :as re-frame]))
+            [re-frame.core :as re-frame]
+            [bulb.views.app :refer [app]]))
 
 
-(defn app []
-  [:div
-   "Ready"])
-
-
-(defn mount-root []
+(defn mount []
   (re-frame/clear-subscription-cache!)
   (reagent/render [app]
-                  (.getElementById js/document "root")))
+                  (.getElementById js/document "core")))
 
-(mount-root)
 
-#_(defn ^:export initialise []
-  (enable-console-print!)
-  (mount-root))
+(enable-console-print!)
+(mount)
