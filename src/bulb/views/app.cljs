362958f0942382906fa16e2973083376320737a2
Keigo Suzukawa <k.suzukawa@gmail.com>
2019-09-28 21:13:42 +0100

    Minor updates

--- src/bulb/views/app.cljs
+++ src/bulb/views/app.cljs
@@ -1,13 +1,24 @@
-(ns bulb.views.app)
+(ns bulb.views.app
+  (:require [re-frame.core :as re-frame]))
 
 
-(defn view
-  []
+(defn view [{:keys [authorised?]}
+            {:keys [toggle route]}]
   [:div
    {:class "app"}
-   "App Ready!"])
+   (if authorised? "authed" "not authed")
+   [:button
+    {:on-click toggle}
+    "toggle!"]
+   [:button
+    {:on-click route}
+    "route!"]])
 
 
-(defn app
-  []
-  [view])
+(defn app []
+  (let [!authorised? (re-frame/subscribe [:authorised?])]
+    (fn []
+      [view
+       {:authorised? @!authorised?}
+       {:toggle #(re-frame/dispatch [:toggle])
+        :route #(re-frame/dispatch [:go-to-home-page])}])))
