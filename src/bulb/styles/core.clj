c914e70a29461f8d4b6586e1369821fcb244c2e1
Keigo Suzukawa <k.suzukawa@gmail.com>
2020-01-14 07:59:28 +0000

    Add base styling

--- src/bulb/styles/core.clj
+++ src/bulb/styles/core.clj
@@ -1,7 +1,59 @@
 (ns bulb.styles.core
-  (:require [garden.def :refer [defstyles]]))
+  (:require [bulb.styles.constants :as c]
+            [bulb.styles.fonts :as fonts]
+            [bulb.styles.components.text :refer [text]]
+            [bulb.styles.components.icon :refer [icon]]
+            [bulb.styles.components.cell :refer [cell]]
+            [bulb.styles.components.button :refer [button]]
+            [bulb.styles.components.page :refer [page]]
+            [bulb.styles.components.notification :refer [notification]]
+            [garden.def :refer [defstyles]]
+            [garden.units :refer [px percent ms]]
+            [normalize.core :refer [normalize]]))
 
 
 (defstyles core
-  [:body {:font-size "10px"
-          :color :black}])
+
+  ;; third party css
+  [normalize]
+
+  ;; foundations
+  [:*
+   {:box-sizing :border-box
+    :list-style-type :none
+    :margin 0
+    :padding 0}]
+
+  [:textarea
+   {:outline :none
+    :overflow :auto
+    :-webkit-box-shadow :none
+    :-moz-box-shadow :none
+    :box-shadow :none
+    :resize :none
+    :padding (px (:xx-small c/spacing))
+    :border [[:solid (px (:xxx-tiny c/filling)) (:white-three c/colour)]]
+    :font-family "\"Open Sans\", sans-serif"
+    :font-size (px (:medium c/font-size))
+    :font-weight 400
+    :line-height 1.3
+    :font-variant :normal
+    :text-transform :none
+    :-webkit-font-smoothing :antialiased
+    :-moz-osx-font-smoothing :grayscale
+    :text-decoration :none}]
+
+  ["textarea::placeholder"
+   {:color (:grey-two c/colour)}]
+
+  ;; fonts
+  fonts/icomoon
+
+  ;; components
+  text
+  icon
+  cell
+  button
+  page
+  notification
+  )
