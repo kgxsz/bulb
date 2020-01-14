9fef6fb02a455d735b38913e37dc32725cafec8b
Keigo Suzukawa <k.suzukawa@gmail.com>
2020-01-14 07:59:49 +0000

    Update main page

--- src/bulb/views/core.cljs
+++ src/bulb/views/core.cljs
@@ -1,39 +1,61 @@
 (ns bulb.views.core
-  (:require [re-frame.core :as re-frame]))
+  (:require [re-frame.core :as re-frame]
+            [bulb.views.notification :as notification]
+            [bulb.utils :as u]))
 
 
-(defn view [{:keys [routing-initialised? route authorised?]}
-            {:keys [initialise-authorisation]}]
-  (if routing-initialised?
-    [:div
-     {:class "core"}
-     (case route
-       :home [:div
-              "Hi there!"
+(defn view [{:keys [route authorised?]}
+            {:keys [error-notification ]}
+            {:keys [authorise deauthorise get-profile]}]
+  [:div
+   {:class (u/bem [:core])}
+   [error-notification]
+   [:div
+    {:class (u/bem [:page]
+                   [:cell :overflow-auto :colour-white-one])}
+    (case route
+      :home [:div
+              [:button
+               {:on-click get-profile}
+               [:div
+                {:class (u/bem [:text])}
+                "see my profile"]]
               (if authorised?
-                [:div
-                 "authed"]
                 [:button
-                 {:on-click initialise-authorisation}
+                 {:on-click deauthorise}
+                 "deauthorise"]
+                [:button
+                 {:on-click authorise}
                  "authorise"])]
-       :user [:div
-              "You're a user!"]
-       :authorisation [:div
-                       "authorising"]
-       :unknown [:div
-                 "Something is wrong"])]
-    [:div
-     {:class "core"}
-     "Not ready"]))
+      :user [:div
+             {:class (u/bem [:text])}
+             "You're a user!"]
+      :authorisation [:div
+                      {:class (u/bem [:text])}
+                      "authorising"]
+      :unknown [:div
+                {:class (u/bem [:text])}
+                "Something is wrong"]
+      [:div
+       {:class (u/bem [:cell :margin-top-x-huge])}
+       [:div
+        {:class (u/bem [:text :align-center :font-size-large])}
+        "Loading"]])]])
 
 
 (defn core []
-  (let [!routing-initialised? (re-frame/subscribe [:routing-initialised?])
-        !route (re-frame/subscribe [:route])
+  (let [!route (re-frame/subscribe [:route])
         !authorised? (re-frame/subscribe [:authorised?])]
     (fn []
       [view
-       {:routing-initialised? @!routing-initialised?
-        :route @!route
+       {:route @!route
         :authorised? @!authorised?}
-       {:initialise-authorisation #(re-frame/dispatch [:initialise-authorisation])}])))
+       {:error-notification notification/error-notification}
+       {:authorise #(re-frame/dispatch [:authorise])
+        :deauthorise #(re-frame/dispatch [:deauthorise])
+        :get-profile #(re-frame/dispatch [:get-profile])}])))
+
+
+;; Loading until a profile has been procured
+;; Error messages for any non-200 responses
+;; If profile is nil, show auth button
