7a06a69373441ded63c291eff896c526c40a35ed
Keigo Suzukawa <k.suzukawa@gmail.com>
2020-01-16 21:22:27 +0000

    Include a generic button

---
+++ src/bulb/views/button.cljs
@@ -0,0 +1,34 @@
+(ns bulb.views.button
+  (:require [re-frame.core :as re-frame]
+            [bulb.utils :as u]))
+
+
+(defn view [{:keys [type disabled? label]}
+            {:keys [on-click]}]
+  [:div
+   {:class (u/bem [:button type (when disabled? :disabled)]
+                  [:cell :row :width-huge :height-x-large])
+    :on-click (when-not disabled? on-click)}
+   [:div
+    {:class (u/bem [:text :font-size-medium])}
+    label]])
+
+
+(defn button [properties behaviours]
+  [view
+   properties
+   behaviours])
+
+
+(defn primary-button [properties behaviours]
+  [button
+   (assoc properties
+          :type :primary)
+   behaviours])
+
+
+(defn secondary-button [properties behaviours]
+  [button
+   (assoc properties
+          :type :secondary)
+   behaviours])
