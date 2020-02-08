b2ad9ac8a63dcd9141522d08feff9650bdf4b576
Keigo Suzukawa <k.suzukawa@gmail.com>
2020-02-08 13:11:10 +0000

    Get a naive grid in place

--- src/bulb/styles/components/grid.clj
+++ src/bulb/styles/components/grid.clj
@@ -1,22 +1,38 @@
 (ns bulb.styles.components.grid
   (:require [bulb.styles.constants :as c]
+            [bulb.styles.utils :as u]
             [garden.def :refer [defstyles]]
             [garden.units :refer [px percent ms vh vw]]))
 
 (defstyles grid
   [:.grid
-   [:&__header]
+
+   [:&__header
+    [:&__separator
+     {:padding [[0 (-> c/spacing :x-small px)]]}]]
+
    [:&__body
-    {:height (-> c/filling :x-huge px)
-     :margin-top (-> c/spacing :x-large px)
-     :background-color (:white-three c/colour)}]
+    {:position :relative
+     :overflow :hidden
+     :height (-> c/filling :x-huge px)
+     :margin-top (-> c/spacing :x-large px)}]
+
+   [:&__cells
+    {:position :absolute
+     :top 0
+     :right 0
+     :display :grid
+     :grid-template-rows [(repeat 7 (-> c/filling :x-small px))]
+     :grid-auto-columns (-> c/filling :x-small px)
+     :grid-auto-flow :column
+     :grid-gap (-> c/spacing :xx-tiny px)}
+    [:&__cell
+     {:border-radius (-> c/radius :tiny px)
+      :cursor :pointer}
+     [:&--colour
+      (u/make-modifiers c/colour :background-color)]]]
+
    [:&__footer
     {:height (-> c/filling :xx-tiny px)
      :margin-top (-> c/spacing :x-large px)
      :background-color (-> c/colour :white-three)}]])
-
-;; Grid widths fixed
-;; 1004
-;; 748
-;; 460
-;; 300
--- src/bulb/views/grid.cljs
+++ src/bulb/views/grid.cljs
@@ -7,17 +7,54 @@
 
 (defn view [{:keys [logo primary-button]}
             {:keys [update-route]}]
-  [:div
-   {:class (u/bem [:grid])}
-   [:div
-    {:class (u/bem [:grid__header])}
+  (let [title "Title"
+        subtitle "some long winded subtitle about this"]
     [:div
-     {:class (u/bem [:text :ellipsis :font-size-huge :font-weight-bold])}
-     "Title - subtitle"]]
-   [:div
-    {:class (u/bem [:grid__body])}]
-   [:div
-    {:class (u/bem [:grid__footer])}]])
+     {:class (u/bem [:grid])}
+     [:div
+      {:class (u/bem [:grid__header])}
+      [:span
+       {:class (u/bem [:text :font-size-huge :font-weight-bold :colour-black-two])}
+       title]
+      [:span
+       {:class (u/bem [:grid__header__separator])}
+       [:span
+        {:class (u/bem [:text :font-size-huge :colour-grey-two])}
+        "—"]]
+      [:span
+       {:class (u/bem [:text :font-size-huge :colour-grey-two])}
+       subtitle]]
+     [:div
+      {:class (u/bem [:grid__body])}
+      [:div
+       {:class (u/bem [:grid__cells])}
+       (doall
+        (for [i (range 500)]
+          [:div
+           {:key i
+            :title (str i)
+            :class (u/bem [:grid__cells__cell :colour-grey-four])}]))
+       #_(doall
+        (for [{:keys [date label shaded? protected?]} (make-items (t/today))]
+          (let [add-checked-date (fn [] (re-frame/dispatch [:add-checked-date id date]))
+                remove-checked-date (fn [] (re-frame/dispatch [:remove-checked-date id date]))
+                checked? (contains? (set @!checked-dates) date)]
+            [:div
+             {:key date
+              :title label
+              :class (u/bem [:calendar__items__item
+                             (cond
+                               checked? colour
+                               shaded? :colour-grey-medium
+                               :else :colour-grey-light)])
+              :on-click (when-not protected?
+                          (if checked?
+                            remove-checked-date
+                            add-checked-date))}])))]
+
+      ]
+     [:div
+      {:class (u/bem [:grid__footer])}]]))
 
 
 (defn grid []
